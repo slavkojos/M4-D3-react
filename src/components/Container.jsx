584c5a54f872e788a2f6a2f956a88403609b08fa
slavko <slavkoj6@gmail.com>
2021-03-24 16:50:08 +0100

    init commit

---
+++ src/components/Container.jsx
@@ -0,0 +1,10 @@
+import { Box } from '@chakra-ui/react';
+import { Center, Square, Circle } from '@chakra-ui/react';
+
+export default function Container() {
+  return (
+    <Box bg="gray.200" w="100%" p={2}>
+      <Center></Center>
+    </Box>
+  );
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+import Product from './Product';
+import Container from './Container';
+
+export default function Main() {
+  return (
+    <Container>
+      <Product />
+    </Container>
+  );
+}
